e16e1f5dba59f0e97c4b64a8080ab9c28fcd1987
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-19 10:40:50 +0530

    Setup the basic reset and styling

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix Clone</title>
+      </Head>
+
+      <h1>Netflix Clone</h1>
+    </div>
+  );
+}
