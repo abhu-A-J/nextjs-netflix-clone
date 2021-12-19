ad171fab7f589ea9c346edcdb265a7fa50047522
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-19 12:07:34 +0530

    Complete the navbar component

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,34 +1,72 @@
+import Image from 'next/image';
+import Link from 'next/Link';
+import { useState } from 'react';
+
 import styles from './index.module.css';
 
 export default function Navbar(props) {
   // Props
   const { userName } = props;
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown((prevState) => !prevState);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <a href="/" className={styles.logoLink}>
-          <div className={styles.logoWrapper}>Netflix</div>
-        </a>
+        <Link href="/" passHref>
+          <a className={styles.logoLink}>
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/netflixLogo.svg"
+                alt="Netflix Logo"
+                width={128}
+                height={34}
+              />
+            </div>
+          </a>
+        </Link>
 
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>My List</li>
+          <li className={styles.navItem}>
+            <Link href="/" passHref>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className={styles.navItem2}>
+            <Link href="/browse/my-list" passHref>
+              <a>My List</a>
+            </Link>
+          </li>
         </ul>
 
         {/* Navigation */}
         <nav className={styles.navContainer}>
           <div>
-            <button className={styles.usernameBtn}>
+            <button className={styles.usernameBtn} onClick={toggleDropdown}>
               <p className={styles.username}>{userName}</p>
-              {/* Expanded More icon */}
+              <Image
+                src="/expandeMore.svg"
+                alt="Expande the dropdown to see the options"
+                width={24}
+                height={24}
+              />
             </button>
 
-            <div className={styles.navDropdown}>
-              <div>
-                <a className={styles.linkName}>Sign Out of Netflix</a>
-                <div className={styles.lineWrapper} />
+            {showDropdown && (
+              <div className={styles.navDropdown}>
+                <div>
+                  <Link href="/login" passHref>
+                    <a className={styles.linkName}>Sign Out of Netflix</a>
+                  </Link>
+                  <div className={styles.lineWrapper} />
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </nav>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Banner
         title="Spiderman"
         subTitle="Far from home"
-        imageUrl="https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        imageUrl="https://github.com/kulkarniankita/discover-videos/blob/main/public/static/clifford.webp?raw=true"
       />
 
       {/* <Card /> */}
