f4e2e6d85c5e5709a23ee676f7fab3395ce43c12
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-22 16:04:12 +0530

    Remove page change flicker by subscribing to router events and add loader

---
+++ components/Loader/index.jsx
@@ -0,0 +1,5 @@
+import styles from './index.module.css';
+
+export default function Loader() {
+  return <p className={styles.loader}>Loading...</p>;
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,22 @@
 import '../styles/globals.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { magic } from '../lib/magic-client';
 
+import Loader from '../components/Loader';
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  // remove loader once routing is successful
+  const handleComplete = () => {
+    setIsLoading(false);
+  };
+
   // effect to check and redirect based on user login status
   useEffect(async () => {
     const isLoggedIn = await magic.user.isLoggedIn();
@@ -17,6 +27,22 @@ function MyApp({ Component, pageProps }) {
       router.push('/login');
     }
   }, []);
+
+  // listen to events change to stop flicker event
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return <Component {...pageProps} />;
 }
 
