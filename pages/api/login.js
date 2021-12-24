4c94e8a670d459d6722533c1f2e3609ea39d08c2
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-24 11:18:12 +0530

    Clean up login page

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 import { isNewUser, createNewUser } from '../../lib/db/hasura';
 import { magicAdmin } from '../../lib/magic-admin';
+import { setTokenCookie } from '../../lib/cookies';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -10,45 +12,38 @@ export default async function handler(req, res) {
       const authHeader = req.headers.authorization;
       const didToken = authHeader ? authHeader.split(' ')[1] : null;
 
-      if (didToken) {
-        const { email, issuer, publicAddress } =
-          await magicAdmin.users.getMetadataByToken(didToken);
+      // get user details from the did token
+      const { email, issuer, publicAddress } =
+        await magicAdmin.users.getMetadataByToken(didToken);
 
-        // set up payload and hasura cliams
-        const hasuraCliams = {
-          iat: Math.floor(Date.now() / 1000),
-          email,
-          issuer,
-          publicAddress,
-          'https://hasura.io/jwt/claims': {
-            'x-hasura-allowed-roles': ['admin', 'user'],
-            'x-hasura-default-role': 'user',
-            'x-hasura-user-id': issuer,
-          },
-        };
+      // set up payload and hasura cliams
+      const hasuraCliams = {
+        iat: Math.floor(Date.now() / 1000),
+        email,
+        issuer,
+        publicAddress,
+        'https://hasura.io/jwt/claims': {
+          'x-hasura-allowed-roles': ['admin', 'user'],
+          'x-hasura-default-role': 'user',
+          'x-hasura-user-id': issuer,
+        },
+      };
 
-        // create a JWT token
-        const jwtToken = jwt.sign(hasuraCliams, process.env.JWT_SECRET, {
-          expiresIn: '7d',
-        });
+      // create a JWT token
+      const jwtToken = jwt.sign(hasuraCliams, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      });
 
-        // check if user exists
-        const isNewUserQueryResult = await isNewUser(issuer, jwtToken);
+      // check if user exists
+      const isNewUserQueryResult = await isNewUser(issuer, jwtToken);
 
-        // if a new user add them to hasura
-        if (isNewUserQueryResult) {
-          const userCreated = await createNewUser(
-            { email, issuer, publicAddress },
-            jwtToken
-          );
-          res.status(200).json({
-            done: true,
-            msg: 'New user has been created in DB',
-          });
-        } else {
-          res.status(200).json({ done: true, msg: 'User already exist on DB' });
-        }
-      }
+      // if user user then add the user on DB
+      isNewUserQueryResult &&
+        (await createNewUser({ email, issuer, publicAddress }, jwtToken));
+
+      // set the cookie on response
+      const cookie = setTokenCookie(res, jwtToken);
+      res.status(200).json({ done: true });
     } catch (err) {
       console.error('Something went wrong', err);
       res.status(500).json({ done: false });
